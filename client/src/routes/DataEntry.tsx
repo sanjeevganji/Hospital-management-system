72c3ac0c057c563da7a10b8651469923222821e2
Atishay Jain <atishayjain002@gmail.com>
2023-03-07 17:53:52 +0530

    Data

--- client/src/routes/DataEntry.tsx
+++ client/src/routes/DataEntry.tsx
@@ -1,29 +1,49 @@
 import React, { useEffect } from "react";
 import { getUser } from "../log";
+import { getAllPatientsForDataEntry } from "../API";
 
 function DataEntry() {
-    //get the user who is logged in
-    let [user, setUser] = React.useState<any>(null);
-    useEffect(() => {
-      getUser().then((user: any) => setUser(user));
-    }, []);
+  let [patients, setPatients] = React.useState([]);
+  let [pop1, setPop1] = React.useState(null);
+
+  //get the user who is logged in
+  let [user, setUser] = React.useState<any>(null);
+  useEffect(() => {
+    getUser().then((user: any) => setUser(user));
+  }, []);
+
+  useEffect(() => {
+    if (!user) return;
+    console.log({ user });
+    getAllPatientsForDataEntry(user).then((res) => {
+      setPatients(res);
+    });
+  }, [user]);
+
   return (
     <>
-      <div className="fixed inset-0 bg-slate-200 text-gray-700 overflow-y-scroll px-6">
-        <div className="container mx-auto px-2">
-          <div className="grid grid-cols-2 place-items-center">
-            <h1 className="justify-self-start text-gray-500 whitespace-nowrap">
-              Welcome {user[0].username || "no username"}!
-            </h1>
-            <button
-              onClick={async () => {
-                user[1](null);
-              }}
-              className=" justify-self-end red"
-            >
-              log out
-            </button>
-          </div>
+      <div className="px-6">
+        <h2 className="mt-8 mb-2 shadow-lg ">Dashboard</h2>
+        <div className="grid grid-cols-6 gap-3 mb-2 mt-4 text-center">
+          <h3 className="col-span-1">Patient Name</h3>
+          <h3 className="col-span-4">ID</h3>
+          <h3 className="col-span-1">Add</h3>
+        </div>
+        <div className="flex flex-col gap-3 whitespace-nowrap mb-8">
+          {patients.map((patients: any) => (
+            <div className="grid grid-cols-6 gap-3" key={patients.appID}>
+              <div className="card col-span-1">{patients.pID}</div>
+              <div className="card col-span-4">{patients.Name}</div>
+              <button
+                onClick={async () => {
+                  setPop1(patients.pID);
+                }}
+                className="blue"
+              >
+                Enter
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </>
